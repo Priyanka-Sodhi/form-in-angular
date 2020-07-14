92b1be1a2699ebb5c4cf9c8833e0d7bcfa9d0a56
Priyanka-Sodhi <priyankasodhi00@gmail.com>
2020-07-14 23:51:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# form-in-angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/form-in-angular)
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TformComponent } from './tform/tform.component';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, TformComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
